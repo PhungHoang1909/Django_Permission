87ce05783824c16c74c80e93c2f4a0b41e2516a5
phung hoang <lnphunghoang190903@gmail.com>
2024-06-16 08:58:42 +0700

    Module Permission

--- 2_ObjectLevel_AdminSite/inventory/admin.py
+++ 2_ObjectLevel_AdminSite/inventory/admin.py
@@ -5,4 +5,10 @@ from guardian.admin import GuardedModelAdmin
 @admin.register(Product)
 class ProductAdmin(GuardedModelAdmin):
     list_display = ('name',)
-    
+
+    def has_module_permission(self, request):
+        if super().has_module_permission(request):
+            return True 
+        
+    def get_queryset(self, request):
+        return super().get_queryset(request)
