584c5a69bb006ca32e7a8cdf78cb0191ed800f32
phung hoang <lnphunghoang190903@gmail.com>
2024-06-16 08:05:11 +0700

    Admin Class

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+# admin.site.register(Product);
+
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ("name", )
+
+#     def get_from(self, request, obj=None, **kwargs):
+#         form = super().get_form(request, obj, **kwargs)
+#         is_superuser = request.user.is_superuser
+        
+#         if not is_superuser:
+#             form.base_fields['name'].disabled = True
+
+#         return form
+    
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+
+        if not is_superuser:
+            form.base_fields['name'].disabled = True
+        return form
