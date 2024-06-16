3975df0f16385260911972b6a3883453596aba17
phung hoang <lnphunghoang190903@gmail.com>
2024-06-16 09:54:40 +0700

    Authentication

--- 3_ModelLevel_Views_Templates/project/views.py
+++ 3_ModelLevel_Views_Templates/project/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
+from django.contrib.auth.decorators import login_required
+
 from .forms import ProjectForm
 from .models import Project
 
@@ -9,14 +11,17 @@ from .models import Project
 def index(request):
     return render(request, "index.html")
 
+@login_required
 def project_listing(request):
     project_data = Project.objects.all()
     return render(request, "project.html", {"projects": project_data})
 
+@login_required
 def project_detail(request, id):
     project = get_object_or_404(Project, slug=id)
     return render(request, "detail.html", {"detail": project})
 
+@login_required
 def create_project(request):
 
     if request.method == "POST":
