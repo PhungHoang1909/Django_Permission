02839fa5a624d77cb65bcac2092baae28977bf06
phung hoang <lnphunghoang190903@gmail.com>
2024-06-16 09:12:55 +0700

    Change Permission

--- 2_ObjectLevel_AdminSite/inventory/admin.py
+++ 2_ObjectLevel_AdminSite/inventory/admin.py
@@ -16,13 +16,21 @@ class ProductAdmin(GuardedModelAdmin):
     
     def has_permission(self, request, obj, action):
         opts = self.opts
+        code_name = f'{action}_{opts.model_name}'
+        if obj:
+            return request.user.has_perm(f'{opts.app_label}.{code_name}', obj)
+        else:
+            return True
+        
+    def get_model_objects(self, request, action=None, klass=None):
+        pass
 
     
     def has_view_permission(self, request, obj=None):
-        return False
+        return self.has_permission(request, obj, 'view')
     
     def has_change_permission(self, request, obj=None):
-        return False
+        return self.has_permission(request, obj, 'change')
     
     def has_delete_permission(self, request, obj=None):
-        return False
+        return self.has_permission(request, obj, 'delete')
